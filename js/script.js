e1e2d6c375323a21f0b6ec69fb7eae86ee1ebe5e
amanga <CAMANGA@bunge.com>
2017-04-28 11:52:05 -0400

    Refactoring
    Part of previous commit

--- js/script.js
+++ js/script.js
@@ -26,30 +26,3 @@ atcgtekApp.config(function($routeProvider){
 		,controller : 'contactController'
 	});
 });
-
-
-// create the controller and inject Angular's $scope
-atcgtekApp.controller('mainController', function($scope) {
-
-	// create a message to display in our view
-	$scope.message = 'Everyone come and see how good I look!';
-});
-
-atcgtekApp.controller('aboutController', function($scope) {
-
-	// create a message to display in our view
-	$scope.message = 'Look! Iam an about page.';
-});
-
-
-atcgtekApp.controller('resourcesController', function($scope) {
-
-	// create a message to display in our view
-	$scope.message = 'List of Resources!. This is just a demo.';
-});
-
-atcgtekApp.controller('contactController', function($scope) {
-
-	// create a message to display in our view
-	$scope.message = 'Contact us! Jack. This is just a demo.';
-});
