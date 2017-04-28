675863f66348f330b1c2a43932baaa8135a16468
amanga <CAMANGA@bunge.com>
2017-04-28 11:51:22 -0400

    Refactoring
    + mac-controller.js, resource-controller.js
    Modified
    index.html, home.html, resources.html

---
+++ js/mac-controller.js
@@ -0,0 +1,21 @@
+// mac-controller.js
+
+//Controller scirpt file for Home, About & Contact pages
+// create the controller and inject Angular's $scope
+atcgtekApp.controller('mainController', function($scope) {
+
+	// create a message to display in our view
+	$scope.message = 'Everyone come and see how good I look!';
+});
+
+atcgtekApp.controller('aboutController', function($scope) {
+
+	// create a message to display in our view
+	$scope.message = 'Look! Iam an about page.';
+});
+
+atcgtekApp.controller('contactController', function($scope) {
+
+	// create a message to display in our view
+	$scope.message = 'Contact us! Jack. This is just a demo.';
+});
---
+++ js/resource-controller.js
@@ -0,0 +1,9 @@
+// resource-controller.js
+
+atcgtekApp.controller('resourcesController', function($scope) {
+	// create a message to display in our view
+	$scope.message = 'List of Resources!. This is just a demo.';
+
+	
+});
+
